6627ebe0b6f57f129982a0fad1f7bee3d546c708
devChuk <brian_chuk@yahoo.com>
2018-11-30 13:22:48 -0500

    Add generateHashes example config & output

---
+++ securebrowse.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    filePaths: [
+        'a.css',
+        'a.js',
+        'b.js',
+        'c.js'
+        // ['c.js', 'anotherVersionForABTesting.js']
+    ],
+    maxTXTLength: 255
+};
