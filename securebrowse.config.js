a27c50e2bca6000e529bac035e649bbc9941059e
devChuk <brian_chuk@yahoo.com>
2019-03-01 14:55:57 -0500

    Reconfigured test files for absolute instead of relative filePaths

--- securebrowse.config.js
+++ securebrowse.config.js
@@ -1,10 +1,40 @@
 module.exports = {
-    filePaths: [
-        'a.css',
-        'a.js',
-        'b.js',
-        'c.js'
-        // ['c.js', 'anotherVersionForABTesting.js']
-    ],
-    maxTXTLength: 255
+    maxTXTLength: 255,
+    sriHashingAlgorithm: 'sha256',
+    source: '.',
+    destination: '.',
+    txtRecords: [
+        {
+            urlPath: 'testsecurebrowse.org/a.css',
+            filePaths: ['a.css']
+        },
+        {
+            urlPath: 'testsecurebrowse.org/a.js',
+            filePaths: ['a.js']
+        },
+        {
+            urlPath: 'testsecurebrowse.org/non-matching-sri/a.js',
+            filePaths: ['./non-matching-sri/a.js']
+        },
+        {
+            urlPath: 'testsecurebrowse.org/bad-sri/a.js',
+            filePaths: ['./bad-sri/a.js']
+        },
+        {
+            urlPath: 'testsecurebrowse.org/b.js',
+            filePaths: ['./b.js']
+        },
+        {
+            urlPath: 'testsecurebrowse.org/c.js',
+            filePaths: ['./c.js']
+        },
+        {
+            urlPath: 'testsecurebrowse.org/d.js',
+            filePaths: ['./d.js']
+        },
+        {
+            urlPath: 'testsecurebrowse.org/good.html',
+            filePaths: ['./good.html']
+        },
+    ]
 };
